14bccff1028cdd12e922cf28a6b99f737c2a12d5
chandan yadav <chandanky.23@gmail.com>
2020-11-16 23:29:46 +0100

    Adding signup, signin, signout and current user routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,17 @@
-import express from 'express'
-import { json } from 'body-parser'
+import express from "express"
+import { json } from "body-parser"
+
+import { currentUserRouter } from "./routes/currentUser"
+import { signInRouter } from "./routes/siginin"
+import { signUpRouter } from "./routes/signup"
+import { signOutRouter } from "./routes/signout"
 
 const app = express()
 app.use(json())
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hello from auth')
-})
+app.use(currentUserRouter)
+app.use(signInRouter)
+app.use(signUpRouter)
+app.use(signOutRouter)
 
-app.listen(3000, () => console.log('listening on port 3000!'))
+app.listen(3000, () => console.log("listening on port 3000!"))
