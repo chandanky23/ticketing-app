10eb641cb400a9fc24efb4ecffb89566bef470d6
chandan yadav <chandanky.23@gmail.com>
2020-11-21 16:16:29 +0100

    Adding a infra design of auth service and methods, adding a deployment service in kubernetes for mongo db using docker image of mongo from docker hub, adding mongoose, connecting to mongodb with mongoose

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import 'express-async-errors'
 import { json } from "body-parser"
+import mongoose from 'mongoose'
 
 import { currentUserRouter } from "./routes/currentUser"
 import { signInRouter } from "./routes/siginin"
@@ -23,4 +24,20 @@ app.all('*', async () => {
 
 app.use(errorHandler)
 
-app.listen(3000, () => console.log("listening on port 3000!"))
+// Connection to mongodb, db name is defined in srv file in k8s , i.e, auth-mongo-depl.yaml
+const start = async () => {
+  try{
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+    console.log('connected to MongoDb')
+  } catch(err) {
+    console.error(err)
+  }
+}
+
+app.listen(3000, () => console.log("listening on port 3000!!"))
+
+start()
