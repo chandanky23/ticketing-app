a60d72b5cddb0fecfa821852e4cd07624a50d36b
chandan yadav <chandanky.23@gmail.com>
2020-11-22 18:44:03 +0100

    Adding logic to get details of the current user using the jwt token

--- auth/src/routes/currentUser.ts
+++ auth/src/routes/currentUser.ts
@@ -1,9 +1,18 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from "express"
+import jwt from "jsonwebtoken"
 
 const router = express.Router()
 
-router.get('/api/users/currentUser', (req: Request, res: Response) => {
-  res.send('Hello from current user')
+router.get("/api/users/currentUser", (req: Request, res: Response) => {
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null })
+  }
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
+    res.send({ currentUser: payload })
+  } catch (err) {
+    res.send({ currentUser: null })
+  }
 })
 
 export { router as currentUserRouter }
