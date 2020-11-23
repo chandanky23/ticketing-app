96600e7fb79d5d3a968038feacd338841fe81cb9
chandan yadav <chandanky.23@gmail.com>
2020-11-23 21:01:26 +0100

    Adding tests for current user, signin and signout scenarios, setting Cookie globally before the test starts to access teh cookie values in each test and make assertions

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,14 +1,23 @@
 import { MongoMemoryServer } from "mongodb-memory-server"
 import mongoose from "mongoose"
 import { app } from "../app"
+import request from 'supertest'
 
 let mongo: any
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>
+    }
+  }
+}
 
 /**
  * Create a mongodb in memory server connection for doing db tests at the beginning of all the tests.
  */
 beforeAll(async () => {
-  process.env.JWT_KEY = 'asffhf'
+  process.env.JWT_KEY = "asffhf"
   mongo = new MongoMemoryServer()
   const mongoURI = await mongo.getUri()
 
@@ -30,3 +39,18 @@ afterAll(async () => {
   await mongo.stop()
   await mongoose.connection.close()
 })
+
+// using a global signin function and making it Global to avoid importing in all the test cases.
+// This function will only be applicable to the App in test env.
+global.signin = async () => {
+  const email = "test@test.com"
+  const password = "password"
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({ email, password })
+    .expect(201)
+
+  const cookie = response.get('Set-Cookie')
+  return cookie
+}
