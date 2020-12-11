fa55871525ff5b0d6b67d5270da7f49828f7a129
chandan yadav <chandanky.23@gmail.com>
2020-12-12 00:55:58 +0100

    Removing the error and middlewares from auth folder and moving it to an npm package common and further reusing the package to facilitate code sharing

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,13 +2,12 @@ import express from "express"
 import "express-async-errors"
 import { json } from "body-parser"
 import cookieSession from "cookie-session"
+import { errorHandler, NotFoundError } from "@ckytickets/common"
 
 import { currentUserRouter } from "./routes/currentUser"
 import { signInRouter } from "./routes/siginin"
 import { signUpRouter } from "./routes/signup"
 import { signOutRouter } from "./routes/signout"
-import { errorHandler } from "./middlewares/error-handler"
-import { NotFoundError } from "./errors/not-found-error"
 
 const app = express()
 // We want express to trust our proxy coming over ingress-nginx using HTTPS
