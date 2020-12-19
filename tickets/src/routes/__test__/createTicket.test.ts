86dc9b26403886a76e79bc0e51577898a2c62320
chandan yadav <chandanky.23@gmail.com>
2020-12-19 22:59:24 +0100

    Adding express validator to createticket api

--- tickets/src/routes/__test__/createTicket.test.ts
+++ tickets/src/routes/__test__/createTicket.test.ts
@@ -12,15 +12,50 @@ it("can only be accessed if user is signed in", async () => {
 })
 
 it("returns a status other than 401 if the user is signed in", async () => {
-  const response = await request(app).post("/api/tickets").set('Cookie', global.signin()).send({})
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({})
 
   expect(response.status).not.toEqual(401)
 })
 
-it("has a route handler listening to /api/tickets for POST request", async () => {})
+it("return an error if an invalid title is provided", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "",
+      price: 0,
+    })
+    .expect(400)
 
-it("return an error if an invalid title is provided", async () => {})
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      price: 0,
+    })
+    .expect(400)
+})
 
-it("returns an error if an invalid price is provided", async () => {})
+it("returns an error if an invalid price is provided", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "mhghjs",
+      price: -10,
+    })
+    .expect(400)
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "jhgfj",
+    })
+    .expect(400)
+})
 
 it("create a ticket with valid parameters", async () => {})
