c2d11b204f634e79d3c2e0557e2922e7f3e5bed8
chandan yadav <chandanky.23@gmail.com>
2020-12-20 17:44:33 +0100

    Adding a route handler for getting all the tickets in the database ticket and also adding a get for the same

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,7 @@ import cookieSession from "cookie-session"
 import { errorHandler, NotFoundError, currentUser } from "@ckytickets/common"
 import { createTicketRouter } from "./routes/createTicket"
 import { getTicketRouter } from './routes/getTicket'
+import { getTicketsRouter } from './routes/getTickets'
 
 const app = express()
 // We want express to trust our proxy coming over ingress-nginx using HTTPS
@@ -20,6 +21,7 @@ app.use(currentUser)
 
 app.use(createTicketRouter)
 app.use(getTicketRouter)
+app.use(getTicketsRouter)
 
 app.all("*", async () => {
   throw new NotFoundError()
